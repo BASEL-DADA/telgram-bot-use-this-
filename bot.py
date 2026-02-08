74b0993b83d051482e6483b4ac8a1ef8b5c8ca6d
Basell Suleiman Dawoud DADA <basiljzar911@gmail.com>
2026-02-08 19:45:36 +0000

    إضافة xoool51874 لقائمة PoweredSteamBot

--- bot.py
+++ bot.py
@@ -64,7 +64,8 @@ POWERED_STEAM_ACCOUNTS = {
     'harzke2', 'simonremol0t', 'dhgpr09228', 'dragon4404', 'bubblematend1972',
     'qrmdxtob6843', 'p7ao0hxls4', 'mattia3431', 'sundimemoli7g', 'lxakeisl',
     'alarmingbystand2', 'danwangbule', 'godofredo57', 'detroit_st',
-    'amanda_reflect738086', 'vmjix46135', 'srwbi52693', 'pd67gxtl', 'witcher3_st'
+    'amanda_reflect738086', 'vmjix46135', 'srwbi52693', 'pd67gxtl', 'witcher3_st',
+    'xoool51874'
 }
 
 waiting_requests = {}
